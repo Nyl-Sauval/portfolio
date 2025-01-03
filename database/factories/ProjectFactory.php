ca7687673465d7dddfdd4a155503b4d2be7ad480
nyl_sauval <nyl_sauval@ens.univ_artois.fr>
2025-01-03 11:47:00 +0100

    :sparkles: Factory Seeder for an example

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Project;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProjectFactory extends Factory
+{
+    protected $model = Project::class;
+
+    public function definition(): array
+    {
+        return [
+            'title' => 'Cardle',
+            'summary' => 'Jeu inspiré de Wordle basé sur des voitures.',
+            'description' => 'Cardle est un jeu où les joueurs devinent la marque et le modèle d\'une voiture à partir d\'une image.',
+            'images' => ['car1.jpg', 'car2.jpg'],
+            'complexity' => 4,
+            'technologies' => ['PHP', 'Laravel', 'TailwindCSS'],
+            'start_date' => now()->subMonths(2),
+            'end_date' => now(),
+            'project_link' => 'https://cardle-game.com',
+            'status' => 'completed',
+            'type' => 'web',
+            'estimated_duration' => 200,
+            'visibility' => true,
+        ];
+    }
+}
