66e81bea9ae9ff4a3788ecaaa1dd9931bb93a600
Nyl-Sauval <156683628+Nyl-Sauval@users.noreply.github.com>
2025-01-07 14:08:04 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,8 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Custom Styles -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ mix('/build/assets/app-CMZf0XIz.css') }}">
+    <script src="{{ mix('/build/assets/app-B6EvBL-d.js') }}"></script>
 </head>
 <body class="font-sans antialiased">
 <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
