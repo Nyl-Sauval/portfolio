7ef754a5bb3edb63d14cc8d3da713eda8a2578fc
Nyl-Sauval <156683628+Nyl-Sauval@users.noreply.github.com>
2025-01-07 12:56:49 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite';
-import laravel from 'vite-plugin-laravel';
+import laravel from 'laravel-vite-plugin';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-    }),
-  ],
-  build: {
-    manifest: true,
-    outDir: 'public/build', // Assure-toi que la sortie est dans le dossier public
-  },
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+    build: {
+        manifest: true,
+        outDir: 'public/build', // Assure-toi que la sortie est dans le dossier public
+    },
 });
