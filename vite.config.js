1b43e5422a1ae33f92f5b025b25e31b4b34f8464
nyl_sauval <nyl_sauval@ens.univ_artois.fr>
2026-01-21 20:06:45 +0100

    Fix deployment

--- vite.config.js
+++ vite.config.js
@@ -8,14 +8,10 @@ export default defineConfig({
                 'resources/css/app.css'
             ],
             refresh: true,
-            buildDirectory: '', // FORCE le manifest dans public/build
         }),
     ],
     build: {
         manifest: true,
-        outDir: 'public/build', // Assure-toi que la sortie est dans le dossier public
-        rollupOptions: {
-            input: 'resources/css/app.css', // assure-toi que Rollup connaît le point d'entrée
-        },
+        outDir: 'public/build',
     },
 });
